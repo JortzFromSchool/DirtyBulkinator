cdecdccb0281e744a650ecba315d1019afa25b38
JortzFromSchool <johnnydortiz@gmail.com>
2021-10-11 10:21:41 -0400

    import error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import Calculator from './scripts/calculator';
+//const Calculator = require("./scripts/calculator");
 document.addEventListener('DOMContentLoaded', () => {
-    require('./scripts/calculator.js');
+    const goalsForm = document.querySelector('.goals-form');
+    const calculator = new Calculator(goalsForm);
 });
-const apiRequest = (query = 'curry') => {
-    fetch(`/api/food-database/v2/parser?searchTerm=${encodeURIComponent(query)}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      })
-  }
+
+// const apiRequest = (query = 'curry') => {
+//     fetch(`/api/food-database/v2/parser?searchTerm=${encodeURIComponent(query)}`)
+//       .then(res => res.json())
+//       .then(data => {
+//         console.log(data);
+//       })
+//   }
--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,26 +1,36 @@
 //grabbing DOM elements to calculate Calories, Protein and Fats
 
-const goalsForm = document.querySelector('.goals-form');
-const sexInput = document.querySelector('input[name="sex"]');
-const goalInput = document.querySelector('input[name="goal"]');
-const activityInput = document.querySelector('input[name="activity-level"]');
-const weightInput = document.querySelector('input[name="weight"]');
-const heightInput = document.querySelector('input[name="height"]');
-const ageInput = document.querySelector('input[name="age"]');
+class Calculator {
+    constructor(goalsForm){
+        this.goalsForm = goalsForm;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.bindEvents();
+    }
 
-function handleSubmit(e) {
-    e.preventDefault();
-    calculateMacros(sexInput.value, goalInput.value, activityInput.value, weightInput.value, heightInput.value, ageInput.value);
-};
+    handleSubmit(e) {
+        const sexInput = document.querySelector('input[name="sex"]');
+        const goalInput = document.querySelector('input[name="goal"]');
+        const activityInput = document.querySelector('input[name="activity-level"]');
+        const weightInput = document.querySelector('input[name="weight"]');
+        const heightInput = document.querySelector('input[name="height"]');
+        const ageInput = document.querySelector('input[name="age"]');
+        e.preventDefault();
+        this.calculateMacros(sexInput.value, goalInput.value, activityInput.value, weightInput.value, heightInput.value, ageInput.value);
+    }
 
-function calculateMacros(sex, goal, activity, weight, height, age) {
-//needs to manipulate the values already on the page. Make them inputs?
- console.log(sex);
- console.log(goal);
- console.log(activity);
- console.log(weight);
- console.log(height);
- console.log(age);
-};
+    calculateMacros(sex, goal, activity, weight, height, age) {
+    //needs to manipulate the values already on the page. Make them inputs?
+    console.log(sex);
+    console.log(goal);
+    console.log(activity);
+    console.log(weight);
+    console.log(height);
+    console.log(age);
+    }
+    
+    bindEvents() {
+        this.goalsForm.addEventListener('submit', handleSubmit);
+    }
+}
 
-goalsForm.addEventListener('submit', handleSubmit);
+module.exports = Calculator;
