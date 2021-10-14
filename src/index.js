25693d8a81985a9ca4e47a31d4c762cc788ab98e
JortzFromSchool <johnnydortiz@gmail.com>
2021-10-14 10:36:12 -0400

    removed dead code and built

--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import Foods from './scripts/foods';
 
 document.addEventListener('DOMContentLoaded', () => {
     const goalsForm = document.querySelector('.goals-form');
-    // console.log("DOM Content Loaded");
-    // console.log(goalsForm);
     const calculator = new Calculator(goalsForm);
     const foods = new Foods();
     const chart = new Chart(foods, calculator);
     foods.loadChart(chart);
 });
-
-
-
-// const apiRequest = (query = 'curry') => {
-//     fetch(`/api/food-database/v2/parser?searchTerm=${encodeURIComponent(query)}`)
-//       .then(res => res.json())
-//       .then(data => {
-//         console.log(data);
-//       })
-//   }
